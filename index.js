0e70cd03a2a6afe9c0acbdb36d34ee094b74367a
Danish <dkhans2001@gmail.com>
2025-05-17 21:24:20 -0500

    merge

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const array of args) {
+    combinedObject.users.push(...array);
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
 
 
 module.exports = {
